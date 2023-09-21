26d301c10b11a93ce19410d63fad810b9be05e0e
Lluís Peinado <lluis.peinado@europcar.com>
2023-09-21 11:58:10 +0200

    master: tyding up router main file

--- src/router/index.js
+++ src/router/index.js
@@ -3,18 +3,20 @@ import { createRouter, createWebHashHistory } from 'vue-router'
 import ViewNotes from '@/views/ViewNotes.vue'
 import ViewSteps from '@/views/ViewSteps.vue'
 
+const routes = [
+    {
+    path:'/',
+    name:'notes',
+    component: ViewNotes,
+    },
+    {
+    path:'/steps',
+    name:'steps',
+    component: ViewSteps,
+    },
+];
+
 export const router = createRouter({
     history: createWebHashHistory(),
-    routes:[
-        {
-        path:'/',
-        name:'notes',
-        component: ViewNotes,
-        },
-        {
-        path:'/steps',
-        name:'steps',
-        component: ViewSteps,
-        },
-    ]
+    routes
 });
