e825f3a731cedeca2cdafc8400ffb6ddeab119de
Lluís Peinado <lluis.peinado@europcar.com>
2023-09-21 12:03:02 +0200

    master: installing bulma and first button to check it works

--- src/App.vue
+++ src/App.vue
@@ -2,11 +2,15 @@
 </script>
 
 <template>
-  <router-link to="/">Home</router-link>
-  <router-link to="/steps">Steps page</router-link>
+  <router-link to="/" class="button">Home</router-link>
+  <router-link to="/steps" class="button">Steps page</router-link>
   <RouterView>
 
   </RouterView>
 </template>
+
+<style>
+@import 'bulma/css/bulma.min.css'
+</style>
 
 
